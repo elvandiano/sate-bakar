24403b700871fb62aa9833f6fd72929c4c13d4d5
elvandiano <>
2024-03-14 15:16:21 +0700

    fixing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,5 @@
-// /** @type {import('tailwindcss').Config} */
-//
-//
-// module.exports = {
-//   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-//   darkMode: false, // or 'media' or 'class'
-//   theme: {
-//     extend: {},
-//   },
-//   variants: {
-//     extend: {},
-//   },
-//   plugins: [],
-//   corePlugins: {
-//     preflight: false
-//   }
-// }
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
@@ -26,8 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  corePlugins: {
-    preflight: false
-  }
 }
-
